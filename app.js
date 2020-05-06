c158614f98377b801618ff8783de8f8a981075d9
Lais Monteiro <lmonteiro189@gmail.com>
2020-05-06 22:46:55 +0100

    solved

--- app.js
+++ app.js
@@ -13,13 +13,47 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Add the route handlers here:
+app.get('/',(request, response)=>{
+  response.render('index')
+})
+
+app.get('/beers',(request, response)=>{
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi)
+    response.render('beers', {beer: beersFromApi}) 
+  })
+  .catch(error => console.log(error))
+  })
+
+app.get('/random-beers',(request, response)=>{
+  punkAPI.getRandom()
+  .then(beersFromApi => {
+    console.log('Beers from the database', beersFromApi);
+    response.render('random-beers', {randombeer: beersFromApi})
+  })
+  .catch(error => console.log(error))
+})
+
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.listen(3000, () => console.log('Visit at http://localhost:3000'));
+
+
+// app.get(`/beer/:id`,(request, response)=>{
+//   const id = request.params.id
+//   punkAPI
+//   .getBeer(id)
+//   .then(beerFromApi => {
+//     console.log('Beer from the database: ', beerFromApi);
+//     response.render('beer',{beer: beerFromApi});
+//   })
+//   .catch(error => console.log(error));
+// });
